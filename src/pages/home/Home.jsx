da13c99775380e49635029b855dbee8e344b3eef
Abuh Solomon <abuhsolo05@gmail.com>
2022-09-29 10:25:32 +0100

    Added few features

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
-import Navbar from '../../componet/navbar/Navbar'
+import Announcement from '../../component/Announcement'
+import Navbar from '../../component/Navbar'
+import Slider from '../../component/Slider'
 import './Home.css'
 
 const Home = () => {
   return (
     <div className='home'>
+      <Announcement />
        <Navbar />
+       <Slider />
         </div>
   )
 }
